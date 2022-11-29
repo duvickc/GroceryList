864994bdf4f52dc939e1009bd4c4c7a6c0d780ec
Christian Duvick <duvickc@gmail.com>
2022-11-28 18:42:39 -0600

    Initial commit with bootstrap and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { GroceryListComponent } from './grocery-list/grocery-list.component';
+import { ListItemComponent } from './grocery-list/list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    GroceryListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule
